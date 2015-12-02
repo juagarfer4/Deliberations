34a33849268104c7bbb38d5174151d9828e21dd9
juagarfer4 <juanitogarciaman@gmail.com>
2015-12-02 13:52:37 +0100

    Subida de clases
    Se han subido dos clases que faltaban

---
+++ src/main/java/domain/Thread.java
@@ -0,0 +1,91 @@
+
+package domain;
+import java.util.Collection;
+import java.util.Date;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.SafeHtml;
+import org.springframework.format.annotation.DateTimeFormat;
+@Entity
+@Access(AccessType.PROPERTY)
+@Table(name="hilo")
+public class Thread extends DomainEntity{
+	
+	// Constructors ------------------------------------------------------------
+	
+		public Thread() {
+			super();
+		}
+		
+	
+	// Attributes -------------------------------------------------------------
+	
+	private String title;
+	private Date creationMoment;
+	private String text;
+	
+	
+	@NotBlank
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	@DateTimeFormat(pattern="dd/MM/yyyy HH:mm")
+	public Date getCreationMoment() {
+		return creationMoment;
+	}
+	public void setCreationMoment(Date creationMoment) {
+		this.creationMoment = creationMoment;
+	}
+	
+	@NotBlank
+	@SafeHtml
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+	// Relationships ----------------------------------------------------------
+	
+	private User user;
+	private Collection<Comment> comments;
+	
+	@NotNull
+	@ManyToOne(optional=false)
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	
+	@NotNull
+	@OneToMany(mappedBy="thread")
+	public Collection<Comment> getComments() {
+		return comments;
+	}
+	public void setComments(Collection<Comment> comments) {
+		this.comments = comments;
+	}
+	
+	
+	
+}
