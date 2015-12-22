e8231901de88abdcab0e76dd6d02a6856446ff0f
Roberto <robjimcas@gmail.com>
2015-12-22 13:36:58 +0100

    Borrada importación clase Hilo
    Se ha eliminado la importación a la clase Hilo por desuso y porque está obsoleta.

--- src/test/java/services/ServiceTest.java
+++ src/test/java/services/ServiceTest.java
@@ -18,18 +18,16 @@ import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 
-import utilities.AbstractTest;
 import domain.Comment;
-import domain.Hilo;
-import domain.Token;
 import domain.Rating;
+import domain.Token;
 import domain.User;
 import security.LoginService;
 import security.UserAccount;
+import utilities.AbstractTest;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "classpath:spring/datasource.xml",
-		"classpath:spring/config/packages.xml" })
+@ContextConfiguration(locations = { "classpath:spring/datasource.xml", "classpath:spring/config/packages.xml" })
 @Transactional
 @TransactionConfiguration(defaultRollback = false)
 public class ServiceTest extends AbstractTest {
@@ -38,160 +36,148 @@ public class ServiceTest extends AbstractTest {
 
 	@Autowired
 	private UserService userService;
-	
+
 	@Autowired
 	private ThreadService threadService;
-	
+
 	@Autowired
 	private CommentService commentService;
-	
+
 	@Autowired
 	private LoginService loginService;
+
+	@Autowired
 	private RatingService ratingService;
 
 	// Tests --------------------------------------------------------------
 
-	// Create an user --------------------------------------------------------------
+	// Create an user
+	// --------------------------------------------------------------
 
 	@Test
 	public void testCreateUser() {
 
 		User result;
-		
 
 		result = userService.create("Usuario prueba");
-		
+
 		result.getUserAccount().setPassword("prueba");
 		result.setEmail("prueba@gmail.com");
 		result.setSurname("prueba");
 
 		userService.save(result);
-		
+
 		System.out.println("El usuario ha sido creado correctamente.");
-		
 
 	}
-	
-	
-	
-	
-	// Create a thread --------------------------------------------------------------
 
-		@Test
-		public void testCreateThread() {
-			
-			authenticate("user1");
-			domain.Thread result;
-			
+	// Create a thread
+	// --------------------------------------------------------------
 
-			result = threadService.create();
-			
-			result.setTitle("Titulo prueba");
-			result.setDecription("Texto prueba");
+	@Test
+	public void testCreateThread() {
 
-			
+		authenticate("user1");
+		domain.Thread result;
 
-			threadService.save(result);
-			
-			
-			System.out.println("El hilo ha sido creado correctamente.");
-			unauthenticate();
+		result = threadService.create();
 
-		}
-		
-		// Create a comment --------------------------------------------------------------
+		result.setTitle("Titulo prueba");
+		result.setDecription("Texto prueba");
 
-				@Test
-				public void testCreateComment() {
-					
-					authenticate("user1");
-					Comment result;
-					domain.Thread hilo;
+		threadService.save(result);
 
-					result = new Comment();
-					
-					hilo= threadService.findOne(6);
-					result.setCreationMoment(new Date());
-					
-					result.setThread(hilo);
-					result.setUser(userService.findOneByPrincipal());
-					result.setText("Texto comentario");
-					
+		System.out.println("El hilo ha sido creado correctamente.");
+		unauthenticate();
 
-					commentService.save(result);
-					
-					
-					System.out.println("El comentario ha sido creado correctamente.");
-					unauthenticate();
+	}
 
-				}
-			
-				// List all threads --------------------------------------------------------------
+	// Create a comment
+	// --------------------------------------------------------------
 
-				@Test
-				public void testListAllThreads() {
-					
-					authenticate("user1");
-					
-					Collection<domain.Thread> result;
-					
-					result= threadService.findAll();
-					
-					System.out.println("La lista de hilos es: "+ result);
-					unauthenticate();
+	@Test
+	public void testCreateComment() {
 
-				}
-				
+		authenticate("user1");
+		Comment result;
+		domain.Thread hilo;
 
-				
-				@Test
-				public void testToken() throws JsonParseException, JsonMappingException, MalformedURLException, IOException {
-					
-					
-				ObjectMapper objectMapper;
-				UserAccount userAccount;
-				String tokenToVerify;
-				Token resultOfToken;
-				
-				objectMapper = new ObjectMapper();
-				userAccount = new UserAccount();
-				
-				// Se asignan usuario y contraseña a una cuenta de usuario nueva porque si se recuperase una ya creada incluiría el hash
-				// de la contraseña en lugar de la contraseña en sí
-				userAccount.setUsername("usuario");
-				userAccount.setPassword("usuario");
-				
-				tokenToVerify = loginService.verifyToken(userAccount);
-				
-				resultOfToken = objectMapper.readValue(new URL(
-						"http://deliberations.hol.es/auth/api/checkToken?user="+userAccount+"&token="
-								+ tokenToVerify), domain.Token.class);
-				System.out.println("resultado del token: "+resultOfToken.isValid());
-				
-				Assert.isTrue(resultOfToken.isValid());
-				}
+		result = new Comment();
 
+		hilo = threadService.findOne(6);
+		result.setCreationMoment(new Date());
 
-				// Create rating -----------------------------------------------------------------
-				
-				@Test
-				public void createRating(){
-					authenticate("user1");
-					Integer totalAntes=ratingService.findAll().size();
-					Rating res=ratingService.create();
-					domain.Thread thread=threadService.findOne(7);
-					res.setRate(4);
-					res.setThread(thread);
-					ratingService.save(res);
-					Integer totalDespues=ratingService.findAll().size();
-					Assert.isTrue(totalAntes<totalDespues);
-					unauthenticate();
+		result.setThread(hilo);
+		result.setUser(userService.findOneByPrincipal());
+		result.setText("Texto comentario");
 
-				}
-				
-				
-					
-					
-			
-				}
+		commentService.save(result);
 
+		System.out.println("El comentario ha sido creado correctamente.");
+		unauthenticate();
+
+	}
+
+	// List all threads
+	// --------------------------------------------------------------
+
+	@Test
+	public void testListAllThreads() {
+
+		authenticate("user1");
+
+		Collection<domain.Thread> result;
+
+		result = threadService.findAll();
+
+		System.out.println("La lista de hilos es: " + result);
+		unauthenticate();
+
+	}
+
+	@Test
+	public void testAuthConnection() throws JsonParseException, JsonMappingException, MalformedURLException, IOException {
+
+		ObjectMapper objectMapper;
+		UserAccount userAccount;
+		String tokenToVerify;
+		Token resultOfToken;
+
+		objectMapper = new ObjectMapper();
+		userAccount = new UserAccount();
+
+		// Se asignan usuario y contraseña a una cuenta de usuario nueva porque
+		// si se recuperase una ya creada incluiría el hash
+		// de la contraseña en lugar de la contraseña en sí
+		userAccount.setUsername("usuario");
+		userAccount.setPassword("usuario");
+
+		tokenToVerify = loginService.verifyToken(userAccount);
+
+		resultOfToken = objectMapper.readValue(new URL(
+				"http://deliberations.hol.es/auth/api/checkToken?user=" + userAccount + "&token=" + tokenToVerify),
+				domain.Token.class);
+		System.out.println("resultado del token: " + resultOfToken.isValid());
+
+		Assert.isTrue(resultOfToken.isValid());
+	}
+
+	// Create rating
+	// -----------------------------------------------------------------
+
+	@Test
+	public void createRating() {
+		authenticate("user1");
+		Integer totalAntes = ratingService.findAll().size();
+		Rating res = ratingService.create();
+		domain.Thread thread = threadService.findOne(7);
+		res.setRate(4);
+		res.setThread(thread);
+		ratingService.save(res);
+		Integer totalDespues = ratingService.findAll().size();
+		Assert.isTrue(totalAntes < totalDespues);
+		unauthenticate();
+
+	}
+
+}
